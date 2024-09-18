467acc9f4a160037f6131930c60cf84342a05bb3
Adisson <akeenan360@gmail.com>
2024-09-18 18:56:39 -0400

    first commit

---
+++ NumberSystems.java
@@ -0,0 +1,19 @@
+public class NumberSystems {
+    public static void main(String[] args) {
+        String input = "0101";
+        short num = Short.parseShort(input);
+        System.out.println(num);
+    }
+
+//    public short ConvertToOctal(short num) {
+//
+//    }
+//
+//    public short ConvertToDecimal(short num) {
+//
+//    }
+//
+//    public short ConvertToHex(short num) {
+//
+//    }
+}
